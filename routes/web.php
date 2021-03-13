ef049ce0c1bb83def52f2c2d6ddde16a794e77e9
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-03-13 16:10:30 +0900

    Revert "finished part 77"
    This reverts commit 5f6ecb19efce1fc9e1ba12324ae1e7ec98d47f43.

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/user/dashboard', function () {
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
-
-Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->get('/admin/dashboard', function () {
-    return view('admin.dashboard');
-})->name('admin.dashboard');
