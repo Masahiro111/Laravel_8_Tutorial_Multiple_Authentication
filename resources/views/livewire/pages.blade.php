235c8b7e4dfb94308c99742771256516ed04cc9c
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-03-15 01:00:33 +0900

    2-15:20

--- resources/views/livewire/pages.blade.php
+++ resources/views/livewire/pages.blade.php
@@ -1,17 +1,49 @@
 <div class="p-6">
-    <x-jet-button wire:click="createShowModal">
-        {{ __('Create') }}
-    </x-jet-button>
+    <div class="flex items-center justify-end px-4 py-3 text-right sm:px-6">
+        <x-jet-button wire:click="createShowModal">
+            {{ __('Create') }}
+        </x-jet-button>
+    </div>
 
 
     <!-- Confirmation Modal -->
     <x-jet-dialog-modal wire:model="modalFormVisible">
         <x-slot name="title">
-            {{ __('Delete Account') }}
+            {{ __('Save Page') }}
         </x-slot>
 
         <x-slot name="content">
-            {{ __('Are you sure you want to delete your account? Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+            <div class="mt-4">
+                <x-jet-label for="title" value="{{ __('Title') }}" />
+                <x-jet-input id="title" class="block mt-1 w-full" type="text" name="title"
+                    wire:model.debounce.800ms="title" required />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="slug" value="{{ __('Slug') }}" />
+                <div class="mt-1 flex rounded-md shadow-sm">
+                    <span
+                        class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+                        http://localhost:8000/
+                    </span>
+                    <input wire:model="slug"
+                        class="form-input flex-1 block w-full rounded-none rounded-r-md transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                        placeholder="url-slug">
+                </div>
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="content" value="{{ __('Content') }}" />
+                <div class="rounded-md shadow-sm">
+                    <div class="mt-1 bg-white">
+                        <div class="body-content" wire:ignore>
+                            <trix-editor class="trix-content" x-ref="trix" wire:model.debounce.100000ms="content"
+                                wire:key="trix-content-unique-key"></trix-editor>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </x-slot>
 
         <x-slot name="footer">
@@ -19,9 +51,9 @@
                 {{ __('Cancel') }}
             </x-jet-secondary-button>
 
-            <x-jet-danger-button class="ml-2" wire:click="deleteUser" wire:loading.attr="disabled">
-                {{ __('Delete Account') }}
-            </x-jet-danger-button>
+            <x-jet-button class="ml-2" wire:click="create" wire:loading.attr="disabled">
+                {{ __('Save') }}
+            </x-jet-button>
         </x-slot>
     </x-jet-dialog-modal>
 
