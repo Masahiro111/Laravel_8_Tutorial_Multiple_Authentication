c6e734f5de45e09a208dd2c1c42268403970c363
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-03-14 17:13:11 +0900

    2-10:46

--- app/Http/Livewire/Pages.php
+++ app/Http/Livewire/Pages.php
@@ -4,8 +4,19 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 
+
 class Pages extends Component
 {
+    public $modalFormVisible = false;
+    public $slug;
+    public $title;
+    public $content;
+
+    public function createShowModal()
+    {
+        $this->modalFormVisible = true;
+    }
+
     public function render()
     {
         return view('livewire.pages');
--- resources/views/livewire/pages.blade.php
+++ resources/views/livewire/pages.blade.php
@@ -1,3 +1,28 @@
-<div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+<div class="p-6">
+    <x-jet-button wire:click="createShowModal">
+        {{ __('Create') }}
+    </x-jet-button>
+
+
+    <!-- Confirmation Modal -->
+    <x-jet-dialog-modal wire:model="modalFormVisible">
+        <x-slot name="title">
+            {{ __('Delete Account') }}
+        </x-slot>
+
+        <x-slot name="content">
+            {{ __('Are you sure you want to delete your account? Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:click="$toggle('modalFormVisible')" wire:loading.attr="disabled">
+                {{ __('Cancel') }}
+            </x-jet-secondary-button>
+
+            <x-jet-danger-button class="ml-2" wire:click="deleteUser" wire:loading.attr="disabled">
+                {{ __('Delete Account') }}
+            </x-jet-danger-button>
+        </x-slot>
+    </x-jet-dialog-modal>
+
 </div>
