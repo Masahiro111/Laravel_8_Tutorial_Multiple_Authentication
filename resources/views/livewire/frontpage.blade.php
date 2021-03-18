1a8d1ea4a62b08e27ad198972349a4242621cefd
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-03-18 17:50:18 +0900

    finished part 6

--- resources/views/livewire/frontpage.blade.php
+++ resources/views/livewire/frontpage.blade.php
@@ -1,7 +1,12 @@
 <div>
-    This is a full page livewire conponent
-    The URLSlug
-    <h1>{!! $title !!}</h1>
-    <h2>{!! $content !!}</h2>
-
+    <div class="sm:bg-blue-200 md:text-red-300 lg:bg-yellow-300 border p-5 text-gray-500 bg-gray-200 text-2xl">
+        {{ $title }}</div>
+    <div>
+        <div class="border p-5">
+            {!! $content !!}
+        </div>
+        <div class="border p-5">
+            <img src="{{ 'img/cake.jpg' }}" alt="">
+        </div>
+    </div>
 </div>
